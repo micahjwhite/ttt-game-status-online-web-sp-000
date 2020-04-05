269ab93064c16712ad3cc909cb9bc4fc5f8aeeb8
Micah White <micahjwhite@gmail.com>
2020-04-05 19:18:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -63,11 +63,5 @@ def over?(board)
 end
 
 def winner(board)
-  if win_combination == ["X", "X", "X"]
-    return "X"
-  elsif win_combination == ["O", "O", "O"]
-    return "O"
-  else
-    return nil
-  end
+  
 end
