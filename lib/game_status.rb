f6b0687fd87362c1ed5df9cf78219cc38ea21a58
Micah White <micahjwhite@gmail.com>
2020-04-05 23:07:58 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -65,9 +65,12 @@ end
 def winner(board)
   if won?(board)
     WIN_COMBINATIONS.each do |win_combination|
-      position_1 = board[win_combination[0]]
-      position_2 = board[win_combination[1]]
-      position_3 = board[win_combination[2]]
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+      position_1 = board[win_index_1]
+      position_2 = board[win_index_2]
+      position_3 = board[win_index_3]
       if (position_1 == "X" && position_2 == "X" && position_3 == "X")
         return "X"
       elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
