50cf540d8176ef33d1be2c758ae4e42e0a16982d
Micah White <micahjwhite@gmail.com>
2020-04-05 23:05:47 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -62,18 +62,17 @@ def over?(board)
   end
 end
 
-def winner(array)
-  if won?(array)
+def winner(board)
+  if won?(board)
     WIN_COMBINATIONS.each do |win_combination|
-      position_1 = array[win_combination[0]]
-      position_2 = array[win_combination[1]]
-      position_3 = array[win_combination[2]]
+      position_1 = board[win_combination[0]]
+      position_2 = board[win_combination[1]]
+      position_3 = board[win_combination[2]]
       if (position_1 == "X" && position_2 == "X" && position_3 == "X")
         return "X"
       elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
         return "O"
       else
-        # do nothing
       end
     end
   else
